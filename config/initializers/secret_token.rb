d87b7b495f31b1de4d0eae18aeabbf5c6dc34cc0
David Ynevich <f3game@gmail.com>
2013-07-17 21:16:39 +0400

    Исходный код

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '11986393c0f9913696fedfad750e3a59c94901588e4379aaa0550dfeea55bfec0fd605b628ab0e0e3048ef9e2fe93465e6b7a5a67c940afc6a9dd07d289796a9'
